fcbb065789e16add48a7bcb58c582e986ebb99a8
ayomahansani <ayohansi17@gmail.com>
2025-01-21 20:22:23 +0530

    create staff slice

---
+++ src/models/staff.ts
@@ -0,0 +1,27 @@
+export class Staff {
+    staff_id: string;
+    first_name: string;
+    last_name: string;
+    description: string;
+    gender: string;
+    joined_date: string
+    date_of_birth: string;
+    address: string;
+    contact_number: string;
+    email: string;
+    role: string;
+
+    constructor(staff_id: string, first_name: string, last_name: string, description: string, gender: string, joined_date: string, date_of_birth: string, address: string, contact_number: string, email: string, role: string) {
+        this.staff_id = staff_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.description = description;
+        this.gender = gender;
+        this.joined_date = joined_date;
+        this.date_of_birth = date_of_birth;
+        this.address = address;
+        this.contact_number = contact_number;
+        this.email = email;
+        this.role = role;
+    }
+}
--- src/reducers/StaffSlice.ts
+++ src/reducers/StaffSlice.ts
@@ -1,38 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
+import {Staff} from "../models/staff.ts";
 
-const initialState = {
-    staffs: [
-        {
-            firstName: "Ayoma",
-            lastName: "Hansani",
-            address: "Makola",
-            phone: "0751838333",
-            email: "ayo@gmail.com",
-            joinDate: "03-09-2020",
-            birth: "11-03-2001",
-        },
-    ],
-};
+const initialState: Staff[]  = []
 
 const staffSlice = createSlice({
     name: "staff",
     initialState,
     reducers: {
         addStaff: (state, action) => {
-            state.staffs.push(action.payload);
+            state.push(action.payload);
         },
         updateStaff: (state, action) => {
-            const index = state.staffs.findIndex(
-                (staff) => staff.email === action.payload.email
-            );
+            const index = state.findIndex((staff) => staff.email === action.payload.email);
             if (index !== -1) {
-                state.staffs[index] = action.payload;
+                state[index] = action.payload;
             }
         },
         deleteStaff: (state, action) => {
-            state.staffs = state.staffs.filter(
-                (staff) => staff.email !== action.payload.email
-            );
+            return state.filter((staff) => staff.email !== action.payload.email);
         },
     },
 });
